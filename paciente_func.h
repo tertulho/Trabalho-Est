f286f4133327f817b3f2104e4d48a4bcc547f5c5
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-01-23 19:58:54 -0300

    Add files via upload

---
+++ paciente_func.h
@@ -0,0 +1,14 @@
+#ifndef PACIENT_H
+#define PACIENT_H
+
+typedef struct Paciente Pacient;
+
+Pacient *pt_create(int *id, const char *cpf, const char *nome, int *idade, const char *data_cadast);
+
+
+
+
+
+
+
+#endif
