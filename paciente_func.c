ea104acef1e38e02d99344f1e6c3913b44660516
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-01-23 19:59:26 -0300

    Update paciente_func.c

--- paciente_func.c
+++ paciente_func.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
+#include "paciente_func.h"
+#include <string.h>
+#include <stdlib.h>
 
-typedef struct
+struct Paciente
 {
     int id;
-    float cpf;
-    char nome[50];
+    char cpf[15];
+    char nome[100];
     int idade;
-    char data_nasc[8];
+    char data_cadast[11];
 } Paciente;
+
+Pacient *pt_create(int *id, const char *cpf, const char *nome, int *idade, const char *data_cadast)
+{
+    Pacient *p = (Pacient *)malloc(sizeof(Pacient));
+    p->id = id;
+    strcpy(p->cpf, cpf);
+    strcpy(p->nome, nome);
+    p->idade = idade;
+    strcpy(p->data_cadast, data_cadast);
+    
+    return p;
+}
