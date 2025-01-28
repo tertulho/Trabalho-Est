0c6e6400c36422e4a97bdd6de7411356ff7b9f54
tertulho <tertuliano.s.j@gmail.com>
2025-01-27 21:31:05 -0300

    Update main_trab_est.c

--- main_trab_est.c
+++ main_trab_est.c
@@ -1,29 +1,57 @@
 #include <stdio.h>
 
 int main(){
-    char opcao[] = "0";
+    char opcao = '0';
 
     printf("Bem vindo ao sistema HealtSys!\n");
     char menu[] = "1 - Consultar paciente\n2 - Atualizar paciente\n3 - Remover paciente\n4 - Inserir paciente\n5 - Imprimir lista de pacientes\nQ - Sair";
-    printf("%s\n", menu);
-    while (opcao[0] != 'Q')
-    {
-        if (opcao[0] == '1')
-            printf("função 1");
-        if (opcao[0] == '2')
-            printf("função 2");
-        if (opcao[0] == '3')
-            printf("função 3");
-        if (opcao[0] == '4')
-            printf("função 4");
-        if (opcao[0] == '5')
-            printf("função 5");
+    
+
+    while (opcao != 'Q')
+    {   
         
-        scanf("%c", &opcao[0]);
-        printf("%s\n", opcao);
+        printf("%s\n", menu);
+        
+        scanf(" %c", &opcao);
+        printf("%c\n", opcao);
+        // função linux abaixo, comentar quando for rodar em windows
+        //system("clear");
+       
+
+        switch (opcao)
+        {
+        case '0':{
+            printf("...");
+            break;
+        }
+        case '1':{
+            printf("função 1\n");
+            break;
+        }
+        case '2':{
+            printf("função 2\n");
+            break;
+        }case '3':{
+            printf("função 3\n");
+            break;
+        }case '4':{
+            printf("função 4\n");
+            break;
+        }case '5':{
+            printf("função 5\n");
+            break;
+        }case 'Q':{
+            printf("byby");
+            break;
+        }
+        default:
+            printf("opção invalida\n");
+            break;
+        }
+
+             
     }
     printf("fim de programa");
-    
     return 0;
 
 }
