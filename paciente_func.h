c6c44dc761d77b244511181ce86335fbc3d1557c
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-02-02 21:33:50 -0300

    Update paciente_func.h

--- paciente_func.h
+++ paciente_func.h
@@ -1,14 +1,33 @@
-#ifndef PACIENT_H
-#define PACIENT_H
+#ifndef PACIENTE_H
+#define PACIENTE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-typedef struct Paciente Pacient;
+typedef struct Paciente {
+    int id;
+    char cpf[15];
+    char nome[100];
+    int idade;
+    char data_cadastro[11];
+    struct Paciente *next;
+} Paciente;
 
-Pacient *pt_create(int *id, const char *cpf, const char *nome, int *idade, const char *data_cadast);
+typedef struct {
+    Paciente *first;
+    int next_id;
+} BDPaciente;
 
+void load_banco(BDPaciente *bd, const char *filename);
+void salvar_banco(BDPaciente *bd, const char *filename);
+void consultar_paciente(BDPaciente *bd);
+void atualizar_paciente(BDPaciente *bd);
+void remover_paciente(BDPaciente *bd);
+void inserir_paciente(BDPaciente *bd);
+void imprimir_pacientes(BDPaciente *bd);
 
-
-
-
-
+Paciente* buscar_paciente_por_cpf(BDPaciente *bd, const char *cpf);
+Paciente* buscar_paciente_por_nome(BDPaciente *bd, const char *nome);
+void liberar_lista(BDPaciente *bd);
 
 #endif
