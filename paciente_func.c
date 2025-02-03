b64ddee4dc92e9c804712710658877a4cf09bb5b
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-02-02 21:37:49 -0300

    Update paciente_func.c

--- paciente_func.c
+++ paciente_func.c
@@ -3,23 +3,55 @@
 #include <string.h>
 #include <stdlib.h>
 
-struct Paciente
-{
-    int id;
-    char cpf[15];
-    char nome[100];
-    int idade;
-    char data_cadast[11];
-} Paciente;
+void inserir_paciente(BDPaciente *bd) {
+    Paciente *novo = (Paciente*)malloc(sizeof(Paciente));
+    novo->id = bd->next_id++;
+    printf("Digite o CPF (apenas dígitos): ");
+    scanf(" %[^\n]", novo->cpf);
+    printf("Digite o nome: ");
+    scanf(" %[^\n]", novo->nome);
+    printf("Digite a idade: ");
+    scanf("%d", &novo->idade);
+    printf("Digite a data de cadastro (AAAA-MM-DD): ");
+    scanf(" %[^\n]", novo->data_cadastro);
 
-Pacient *pt_create(int *id, const char *cpf, const char *nome, int *idade, const char *data_cadast)
-{
-    Pacient *p = (Pacient *)malloc(sizeof(Pacient));
-    p->id = id;
-    strcpy(p->cpf, cpf);
-    strcpy(p->nome, nome);
-    p->idade = idade;
-    strcpy(p->data_cadast, data_cadast);
-    
-    return p;
+    novo->next = bd->first;
+    bd->first = novo;
+
+    printf("Registro inserido com sucesso.\n");
+}
+
+void remover_paciente(BDPaciente *bd) {
+    int id;
+    printf("Digite o ID do registro a ser removido: ");
+    scanf("%d", &id);
+
+    Paciente *anterior = NULL;
+    Paciente *atual = bd->first;
+
+    while (atual && atual->id != id) {
+        anterior = atual;
+        atual = atual->next;
+    }
+
+    if (atual) {
+        if (anterior) {
+            anterior->next = atual->next;
+        } else {
+            bd->first = atual->next;
+        }
+        free(atual);
+        printf("Registro removido com sucesso.\n");
+    } else {
+        printf("Paciente não encontrado.\n");
+    }
+}
+
+void imprimir_pacientes(BDPaciente *bd) {
+    Paciente *atual = bd->first;
+    printf("ID | CPF | Nome | Idade | Data Cadastro\n");
+    while (atual) {
+        printf("%d | %s | %s | %d | %s\n", atual->id, atual->cpf, atual->nome, atual->idade, atual->data_cadastro);
+        atual = atual->next;
+    }
 }
