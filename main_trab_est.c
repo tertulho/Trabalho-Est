ae8ae26c3904543c42fb49726636d4625ffaa6dd
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-02-08 11:01:03 -0300

    Update main_trab_est.c

--- main_trab_est.c
+++ main_trab_est.c
@@ -1,57 +1,48 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "paciente_func.h"
 
-int main(){
+#define ARQUIVO_CSV "bd_paciente.csv"
+
+int main() {
+    BDPaciente *bd = ListaBD_create();
+    load_banco(bd, ARQUIVO_CSV);  // carrega pacientes do arquivo CSV
     char opcao = '0';
-
-    printf("Bem vindo ao sistema HealtSys!\n");
     char menu[] = "1 - Consultar paciente\n2 - Atualizar paciente\n3 - Remover paciente\n4 - Inserir paciente\n5 - Imprimir lista de pacientes\nQ - Sair";
+    printf("Bem-vindo ao sistema HealthSys!\n");
     
-
-    while (opcao != 'Q')
-    {   
-        
+    do {
         printf("%s\n", menu);
+        printf("////////////////////////////\n");
+        printf("Escolha uma opção: ");
+        scanf(" %c", &opcao); 
         
-        scanf(" %c", &opcao);
-        printf("%c\n", opcao);
-        // função linux abaixo, comentar quando for rodar em windows
-        //system("clear");
-       
+        switch (opcao) {
+            /*case '1':
+                consultar_paciente(bd); MUDAAAAAR
+                break;*/
+            case '2':
+                atualizar_paciente(bd);
+                break;
+            case '3':
+                remover_paciente(bd);
+                break;
+            case '4':
+                inserir_paciente(bd);
+                break;
+            case '5':
+                imprimir_pacientes(bd);
+                break;
+            case 'Q':
+                salvar_banco(bd, ARQUIVO_CSV);
+                printf("Encerrando o programa...\n");
+                break;
+            default:
+                printf("Opção inválida!\n");
+        }
+    } while (opcao != 'Q');
 
-        switch (opcao)
-        {
-        case '0':{
-            printf("...");
-            break;
-        }
-        case '1':{
-            printf("função 1\n");
-            break;
-        }
-        case '2':{
-            printf("função 2\n");
-            break;
-        }case '3':{
-            printf("função 3\n");
-            break;
-        }case '4':{
-            printf("função 4\n");
-            break;
-        }case '5':{
-            printf("função 5\n");
-            break;
-        }case 'Q':{
-            printf("byby");
-            break;
-        }
-        default:
-            printf("opção invalida\n");
-            break;
-        }
-
-             
-    }
-    printf("fim de programa");
+    free_lista(bd);  // libera memória
+    free(bd);
     return 0;
-
 }
