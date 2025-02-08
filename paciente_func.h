3c2d10cde162346467b603d4bd03679ffc21d4d1
Pedrordsm <101359672+Pedrordsm@users.noreply.github.com>
2025-02-08 11:01:42 -0300

    Update paciente_func.h

--- paciente_func.h
+++ paciente_func.h
@@ -1,33 +1,49 @@
-#ifndef PACIENTE_H
-#define PACIENTE_H
+#ifndef PACIENTE_FUNC_H
+#define PACIENTE_FUNC_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+//STRUCTS//
+// estrutura paciente
 typedef struct Paciente {
-    int id;
-    char cpf[15];
     char nome[100];
+    char cpf[14];
     int idade;
-    char data_cadastro[11];
-    struct Paciente *next;
+    int id;
+    char data_cadastro[50];
 } Paciente;
 
-typedef struct {
-    Paciente *first;
-    int next_id;
-} BDPaciente;
+// estrutura do nó
+typedef struct ListNode {
+    Paciente *info;
+    struct ListNode *next;
+} ListNode;
 
-void load_banco(BDPaciente *bd, const char *filename);
-void salvar_banco(BDPaciente *bd, const char *filename);
-void consultar_paciente(BDPaciente *bd);
+// estrutura banco de dados
+typedef struct {
+    int count;
+    int next_id;
+    ListNode *first;
+} BDPaciente;
+//STRUCTS//
+
+//FUNCOES DO MAIN//
+void inserir_paciente(BDPaciente *bd);
+/*void consultar_paciente(BDPaciente *bd);*/
 void atualizar_paciente(BDPaciente *bd);
 void remover_paciente(BDPaciente *bd);
-void inserir_paciente(BDPaciente *bd);
 void imprimir_pacientes(BDPaciente *bd);
+void free_lista(BDPaciente *bd);
+void load_banco(BDPaciente *bd, const char *nomedoarquivo);
+void salvar_banco(BDPaciente *bd, const char *nomedoarquivo);
+//FUNCOES DO MAIN//
 
+//FUNCOES AUXILIARES//
 Paciente* buscar_paciente_por_cpf(BDPaciente *bd, const char *cpf);
 Paciente* buscar_paciente_por_nome(BDPaciente *bd, const char *nome);
-void liberar_lista(BDPaciente *bd);
-
+Paciente *newPaciente(char nome[], char cpf[], int idade, int id, char data_cadastro[]);
+BDPaciente *ListaBD_create();
+//FUNCOES AUXILIARES//
 #endif
